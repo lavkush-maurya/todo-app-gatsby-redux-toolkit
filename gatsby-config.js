c28ad1ce7d51a85f25ebfcf79d5893fa00e5b948
Lavkush Maurya <lavkushmaurya147@gmail.com>
2023-06-20 17:19:56 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `TodoApp Gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
